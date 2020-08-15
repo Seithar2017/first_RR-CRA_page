bcf558a298449f995e098b8f25eb3dd7cbab909e
Krzysztof Waszkiewicz <kwaszkiewicz94@gmail.com>
2020-08-15 18:12:20 +0200

    Updates

---
+++ precache-manifest.7b75c98700fdb8c3ae66d40c5b734cc2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7d9c2bae264806bca1fd",
+    "url": "/first_RR-CRA_page/static/css/main.ccba3356.chunk.css"
+  },
+  {
+    "revision": "7d9c2bae264806bca1fd",
+    "url": "/first_RR-CRA_page/static/js/main.7d9c2bae.chunk.js"
+  },
+  {
+    "revision": "5c18800ff9c23e143eb9",
+    "url": "/first_RR-CRA_page/static/js/runtime~main.5c18800f.js"
+  },
+  {
+    "revision": "e12795637c47cc4e6448",
+    "url": "/first_RR-CRA_page/static/js/2.e1279563.chunk.js"
+  },
+  {
+    "revision": "70e6670ee746fc59f414fde4fcce93af",
+    "url": "/first_RR-CRA_page/static/media/header1.70e6670e.jpg"
+  },
+  {
+    "revision": "d9d36b65a132d26aed884ad58e4719eb",
+    "url": "/first_RR-CRA_page/static/media/header3.d9d36b65.jpg"
+  },
+  {
+    "revision": "33eca35fceea8311d67358a6629fa9d9",
+    "url": "/first_RR-CRA_page/static/media/header2.33eca35f.jpg"
+  },
+  {
+    "revision": "7bffd7df78548901d387d4130dae5306",
+    "url": "/first_RR-CRA_page/index.html"
+  }
+];
